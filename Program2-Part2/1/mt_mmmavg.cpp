649befe4df809230dc4f946fa5a8cd55fa9d0087
Ash Omaraie <aomarai@ilstu.edu>
2022-04-07 23:53:04 +0000

    Transfer subarrays into a new sorted array

--- Program2-Part2/1/mt_mmmavg.cpp
+++ Program2-Part2/1/mt_mmmavg.cpp
@@ -14,7 +14,6 @@ int calculateMedian(vector<int> *dataHeap);
 int calculateMode(vector<int> *dataHeap);
 void *sortSubarray(void *args);
 int calculateMean(vector<int> *dataHeap);
-int comparator(const void *p1, const void *p2);
 
 struct thread_data 
 {
@@ -62,6 +61,7 @@ int main(int argc, char* argv[])
 
     //Split the vector into subarrays by the number of threads
     thread_data *threadData = new thread_data[numThreads];
+    vector<int> *subIndicies = new vector<int>;
     localN = numInts / numThreads;
     localBegin = 0;
     localEnd = localBegin + localN;
@@ -77,50 +77,83 @@ int main(int argc, char* argv[])
         threadData[i].threadSubEnd = localEnd;
         threadData[i].tid = i;
         threadData[i].threadHeap = dataHeap;
+        subIndicies->push_back(localBegin);
         pthread_create(&threads[i], &attr, sortSubarray, &threadData[i]);
         localBegin = localEnd;
         localEnd = (localEnd + localN < numInts ? localEnd + localN : numInts);
     }
 
     //Join threads after completion and grab required indexes
-    vector<int> *returnIndices;
-    void *temp = NULL;
     for (int i = 0; i < numThreads; i++)
     {
         cout << "Joining thread " << i << endl;
         pthread_join(threads[i],  NULL);
     }
 
-    //Print out indices for testing
-    // for (int i = 0; i < returnIndices->size(); i++)
-    // {
-    //     cout << returnIndices->at(i) << endl;
-    // }
+    // //Print out indices for testing
+    cout << subIndicies->size() << endl;
+    for (int i = 0; i < subIndicies->size(); i++)
+    {
+        cout << subIndicies->at(i) << " ";
+    }
 
-    //Merge the subarrays into a new array
+    //Merge the subarrays into a new array by merging two at a time
+    vector<int> *sortedHeap = new vector<int>[numInts];
+    //Keep going until there are no more indexes to subarrays
+    for (int i = 0; i < subIndicies->size() - 1; i++)
+    {
+        int firstIndex, secondIndex;
+        firstIndex = subIndicies->at(i);
+        secondIndex = subIndicies->at(i + 1);
+
+        //Should start at 0 and run until the second subarray is met, then for loop will increment to the next subarray
+        while (firstIndex < subIndicies->at(i + 1))
+        {
+            //Compare the elements at the front of the subarrays.
+            //If first array's element is smaller, add it. Increment the smaller element's index.
+            if (dataHeap->at(firstIndex) < dataHeap->at(secondIndex))
+            {
+                sortedHeap->push_back(dataHeap->at(firstIndex));
+                firstIndex++;
+            }
+            //If the element at the second index is smaller than the first index, add the second. Increment smaller element's index.
+            else if (dataHeap->at(firstIndex) > dataHeap->at(secondIndex))
+            {
+                sortedHeap->push_back(dataHeap->at(secondIndex));
+                secondIndex++;
+            }
+            //If the two elements are equal, add both and increment both.
+            else if (dataHeap->at(firstIndex) == dataHeap->at(secondIndex))
+            {
+                sortedHeap->push_back(dataHeap->at(firstIndex));
+                sortedHeap->push_back(dataHeap->at(secondIndex));
+                firstIndex++;
+                secondIndex++;
+            }
+        }
+        //Shift to the next subarray.
+    }
+
+    //dataHeap no longer needed, delete
+    delete[](dataHeap);
 
     //Test prints
-    cout << "\nDEBUG STATS\n--------------------------------\nHeap size: " << dataHeap->size() << endl;
-    for (int i = 0; i < 100; i++)
+    cout << "\nDEBUG STATS\n--------------------------------\nHeap size: " << sortedHeap->size() << endl;
+    for (int i = 0; i < 250; i++)
     {
-        cout << dataHeap->at(i) << " ";
+        cout << sortedHeap->at(i) << " ";
     }
-    cout << "\nHeap median: " << calculateMedian(dataHeap) << endl;
-    cout << "Heap mean: " << calculateMean(dataHeap) << endl;
-    cout << "Heap mode: " << calculateMode(dataHeap) << endl;
+    cout << "\nHeap median: " << calculateMedian(sortedHeap) << endl;
+    cout << "Heap mean: " << calculateMean(sortedHeap) << endl;
+    cout << "Heap mode: " << calculateMode(sortedHeap) << endl;
     cout << "# Threads: " << numThreads << endl;
 
     //Remove memory leaks
     delete[](threads);
     delete[](threadData);
-    delete[](dataHeap);
+    delete[](sortedHeap);
 
     return 0;
-}
-
-int comparator(const void *p1, const void *p2)
-{
-    return(*(int*)p1 - *(int*)p2);
 }
 
 //Each thread will call this function to sort their own subarray with heapsort
@@ -128,7 +161,7 @@ void *sortSubarray (void *args)
 {
     thread_data threadData = *(thread_data*) args;
 
-    printf("\nThread ID: %lu\nThread subarray begin: %d\nThread subarray end: %d\n\n", threadData.tid, threadData.threadSubBegin, threadData.threadSubEnd);
+    printf("\nExecuting Thread ID: %lu\nThread subarray begin: %d\nThread subarray end: %d\n\n", threadData.tid, threadData.threadSubBegin, threadData.threadSubEnd);
 
     //Sort the thread's subarray
     sort_heap((threadData.threadHeap->begin() + threadData.threadSubBegin),
@@ -136,8 +169,7 @@ void *sortSubarray (void *args)
 
     printf("Thread %lu has executed\n", threadData.tid);
     
-    //Return the beginning index so we know how to merge arrays
-    //return (void*) threadData.threadSubBegin;
+    return 0;
 }
 
 int calculateMedian(vector<int> *dataHeap)
@@ -163,6 +195,7 @@ int calculateMedian(vector<int> *dataHeap)
     }
 }
 
+//Only functions on a sorted array.
 int calculateMode(vector<int> *dataHeap)
 {
     int curNum = dataHeap->at(0);
